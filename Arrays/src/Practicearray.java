3e39778174126b93dae5f5ad2693150c54072c1e
Asus <Asus@LAPTOP-FFN7PJCM.lan>
2022-03-17 10:08:36 +0530

    First Commit

---
+++ Arrays/src/Practicearray.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Practicearray {
+public static void number()
+{
+	int i;
+	int[] arr=new int[5];
+	//Scanner sc=new Scanner(System.in);
+	System.out.println("Enter 5 numbers");
+	for(i=0;i<5;i++) {
+	//	arr[i]=sc.nextInt();
+	}
+	for(i=0;i<5;i++){
+		System.out.println(arr[i]);
+	}
+	}
+}
